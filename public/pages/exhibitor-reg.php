c73554e7f7983b8102c431ef1780f9c9a2194809
Niraj Shrestha <nirajshrestha539@gmail.com>
2023-12-27 15:28:22 +0545

    khalti modal

--- public/pages/exhibitor-reg.php
+++ public/pages/exhibitor-reg.php
@@ -660,20 +660,8 @@ foreach ($stalls as $stall) {
       </div>
     </div>
   </div>
-  <div class="modal fade" id="khaltiModal" data-bs-keyboard="false" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-lg">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <h5>Khalti</h5>
-        </div>
-
-      </div>
-    </div>
-  </div>
-  <div class="modal fade modal-success" id="successModal" data-bs-keyboard="false" tabindex="-1" aria-hidden="true">
+ 
+  <div class="modal fade modal-success" id="khaltiModal" data-bs-keyboard="false" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-md">
       <div class="modal-content" style="overflow: hidden;">
 
